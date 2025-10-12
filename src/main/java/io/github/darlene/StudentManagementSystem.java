167d55229089932c3db9395b8ab6d636a5d48874
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-12 22:59:04 +0300

    Data validation for null and empty spaces in the name variable

--- src/main/java/io/github/darlene/StudentManagementSystem.java
+++ src/main/java/io/github/darlene/StudentManagementSystem.java
@@ -1,18 +1,34 @@
 package io.github.darlene;
 import java.util.*;
 
-abstract class Person{
-    private String name;
+
+// Making the abstract class public because it is being used across different classes and file// This is a java best praactice.
+public abstract class Person{
+
+    private String name;  // What if this method is null? or empty? or just spaces?
     private int age;
     private String email;
-    private String ID;
+    private String id;
 
     // Constructor for the abstract class
     public Person(String name,int age, String email, String ID){
+        // this.name = name; this constructor alone will throw a null pointer exception
+        //Check if name is null
+        if (name == null){
+            throw new IllegalArgumentException("Null Pointer Exception");
+        }
+        // Check if name is empty   .trim() is meant to remove extra spaces.
+        if (name.trim().isEmpty()){
+            throw new IllegalArgumentException("Empty Pointer Exception");
+        }
+
         this.name = name;
+
+
+
         this.age = age;
         this.email = email;
-        this.ID = ID;
+        this.id = id;
     }
 
     // Getters and setters
@@ -20,15 +36,27 @@ abstract class Person{
         return name;
     }
     public void setName(String name){
+        // Check for null pointer
+        if(name == null){
+            throw new IllegalArgumentException("Null Pointer Exception");
+        }
+        // Check for the presense of white spaces and trims it.
+        if(name.trim().isEmpty()){
+            throw new IllegalArgumentException("Empty Pointer Exception");
+        }
+        // set the name
         this.name = name;
     }
 
+    // Validate age before accepting age.
     public int getAge(){
         return age;
     }
     public void setAge(int age){
         this.age = age;
     }
+
+    // Validate email too....Prevent crashes: If you later do email.contains("@") and email is null → NullPointerException
     public String getEmail(){
         return email;
     }
@@ -36,10 +64,10 @@ abstract class Person{
         this.email = email;
     }
     public String getID(){
-        return ID;
+        return id;
     }
-    public void setID(String ID){
-        this.ID = ID;
+    public void setID(String id){
+        this.id = id;
     }
 
     // Abstract method to be implemented by subclass
@@ -100,13 +128,13 @@ class Student extends Person{
 
     // Method to enroll in a course
     public boolean requestEnrollCourse(CourseManager course){
-        // Checks if the course is available using the isAvailable method in CourseManageer
-        if(CourseManager.course == null){
+        // Checks if the course is available using the isAvailable method in CourseManager
+        if(CourseManager.isAvailable(course)){
             System.out.println("Course does not exist");
             return false;
         }
         // Check if their course limit is full
-        if (CourseManager.){
+        if (CourseManager){
 
         }
 
@@ -191,14 +219,16 @@ class Course {
     private String courseName;
     private String courseCode;
     private Student [] enrolledStudents;
+    private Lecturer lecturer [];
     private int studentCount;
     private static final int  MAX_STUDENTS = 200;
-    private boolean isAvailable;
+    private int courseCount;
 
 
     // Constructors
-    public Course(Student [] enrolledStudents, int studentCount, String courseName, String courseCode){
+    public Course(Student [] enrolledStudents, int studentCount, String courseName, String courseCode, int courseCount){
         this.enrolledStudents = new Student[MAX_STUDENTS];
+        this.courseCount = 0;
         this.studentCount = 0;
         this.courseName = courseName;
         this.courseCode = courseName;
@@ -211,7 +241,6 @@ class Course {
     public String getCourseCode(){
         return courseCode;
     }
-
 
     //Check if it can enroll student in course
     public boolean canEnrollStudent(){
@@ -235,6 +264,8 @@ class Course {
         return false;
     }
 
+    // Method search course by code
+
 
     public void displayInfo(){
         System.out.println("=========================================================");
@@ -254,12 +285,10 @@ class Course {
 
 
 class CourseManager {
-    private String courseName;
-    private String courseCode;
     private Lecturer lecturers [];
-    private Course courses [];
+    private static Course courses [];
     private int  enrolledLecturers;
-    private int enrolledCourses;
+    private static int enrolledCourses;
     private static final int MAX_LECTURERS = 10;
     public static final int MAX_COURSES = 50;
     private boolean isAvailable;
@@ -301,7 +330,7 @@ class CourseManager {
     }
 
     // Check if the course is available
-    public boolean isAvailable(Course course){
+    public static boolean isAvailable(Course course){
         // Check for duplicate course code
         for (int i = 0; i < enrolledCourses; i++){
             if(courses[i].getCourseCode().equals(course.getCourseCode())){
@@ -339,15 +368,30 @@ class CourseManager {
     }
 
     // Method to remove a course // After removing we will have to go a string back in the array..
-    public boolean removeCourse(){
-
+    public boolean removeCourse(Course course){
+        // Find the course in enrolled courses
+        for(int i = 0; i <enrolledCourses; i++){
+            if(courses[i].getCourseCode().equals(course.getCourseCode())){
+                course = courses[i];
+                System.out.println("Found the book;");
+                break;
+            }
+        }
 
 
 
         return true;
     }
-    // Method to update a course
 
+    // Method to update a course
+    public boolean updateCourse(Course courseName, Course courseCode){
+        // Check if the course exists by course code
+        if(){
+
+        }
+
+        return false;
+    }
 
     // Method to search course by code
     public boolean searchCourseByCourseCode(Course course){
@@ -367,8 +411,15 @@ class CourseManager {
     }
 
     //Method to assign course to lecturer
-
-
+    public boolean assignCourseToLecturer(Course course){
+        // Check if the maximum number of lecturers is reached
+        if(enrolledCourses == MAX_LECTURERS){
+            System.out.println("Course slot full can't add more");
+        }
+        courses[enrolledLecturers] = course;
+        enrolledLecturers++;
+        return true;
+    }
 
     //Method to display all existing courses
     public void displayAllCourses(Course course){
@@ -377,17 +428,50 @@ class CourseManager {
         }
     }
 
-    // Method to filter by lecturer
+    // Method to filter a course by lecturer's name
+    public boolean searchCourseByLecturerName(Lecturer lecturer){
+        boolean found = false;
+        for (int i = 0; i < enrolledLecturers; i++){
+            if(lecturers[i].getName().contains(lecturer.getName())){
+                lecturers[i].displayInfo();
+                System.out.println("--------------------------------------");
+                found = true;
+            }
+        }
+
+        if (!found){
+            System.out.println("Lecturer: "+ Arrays.toString(lecturers) + " not found");
+        }
+        return false;
+    }
+
+    // Method to filter by lecturer's ID
+    public boolean searchCourseByLecturerCode(Lecturer lecturer){
+        boolean found = false;
+        for (int i = 0; i < enrolledLecturers; i++){
+            if(lecturers[i].getID().contains(lecturer.getID())){
+                lecturers[i].displayInfo();
+                System.out.println("--------------------------------------");
+                found = true;
+            }
+        }
+
+        if (!found){
+            System.out.println("Lecturer: "+ Arrays.toString(lecturers) + " not found");
+        }
+        return false;
+    }
 
 
     // check if course is for which type of student
 
 
-    // Method to display course information
-
-
-
-
+    // Method to display university course information
+    public void displayInfo(){
+        System.out.println("Lecturers enrolled: " + enrolledLecturers);
+        System.out.println("Enrolled courses: " + enrolledCourses);
+        System.out.println("Existing courses list: " );
+    }
 }
 
 // ==============================================
